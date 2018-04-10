f9d992454c24a4d7167a4b846f42b6524dec2edb
Vytautas <viiskis@gmail.com>
2018-04-10 15:40:50 +0300

    deltaco

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,19 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Form\TaskFormType;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TaskController extends Controller
 {
-
 
     private $entityManager;
     private $taskRepository;
@@ -46,27 +42,7 @@ class TaskController extends Controller
     {
         $task = new Task();
         $task->setTitle('My amazing title');
-        $form = $this->createFormBuilder($task)
-            ->add('title', TextType::class)
-            ->add('status', ChoiceType::class, [
-
-                'choices' => [
-                    'Pending' => Task::STATUS_PENDING,
-                    'In progress' => Task::STATUS_IN_PROGRESS,
-                    'Done' => Task::STATUS_DONE,
-                ]]
-            )
-            ->add('deadline', DateTimeType::class)
-            ->add('priority', ChoiceType::class, [
-
-                'choices' => [
-                    'Low' => Task::PRIORITY_LOW,
-                    'Medium' => Task::PRIORITY_MEDIUM,
-                    'High' => Task::PRIORITY_HIGH,
-                ]]
-            )
-            ->add('Submit', SubmitType::class)
-            ->getForm();
+        $form = $this->createForm(TaskFormType::class, $task);
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
@@ -82,5 +58,48 @@ class TaskController extends Controller
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/task/{id}/edit", name="task.edit")
+     */
+    public function edit($id, Request $request)
+    {
+        $task = $this->taskRepository->find($id);
+        if ($task === null) {
+            throw $this->createNotFoundException("Task #" . $id . " does not exist.");
+        }
+
+        $form = $this->createForm(TaskFormType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+            /** @var Task $data */
+            $data = $form->getData();
+//            $data->setCreateAt(new \DateTime());
+
+            $this->entityManager->persist($data);
+            $this->entityManager->flush();
+
+            return $this->redirect('/task');
+        }
+
+        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/task/{id}/delete", name="task.delete")
+     */
+    public function delete($id)
+    {
+        $task = $this->taskRepository->find($id);
+        if ($task === null) {
+            throw $this->createNotFoundException("Task #" . $id . " does not exist.");
+        }
+        $this->entityManager->remove($task);
+        $this->entityManager->flush();
+
+        return $this->redirect('/task');
     }
 }
---
+++ src/Form/TaskFormType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Task;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('status', ChoiceType::class, [
+
+                    'choices' => [
+                        'Pending' => Task::STATUS_PENDING,
+                        'In progress' => Task::STATUS_IN_PROGRESS,
+                        'Done' => Task::STATUS_DONE,
+                    ]]
+            )
+            ->add('deadline', DateTimeType::class)
+            ->add('priority', ChoiceType::class, [
+
+                    'choices' => [
+                        'Low' => Task::PRIORITY_LOW,
+                        'Medium' => Task::PRIORITY_MEDIUM,
+                        'High' => Task::PRIORITY_HIGH,
+                    ]]
+            )
+            ->add('Submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
